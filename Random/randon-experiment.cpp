e657c408f83d5b557b4c57dbd0341fa400068c66
oxygen-TW <howard2805@gmail.com>
2018-03-11 23:55:56 +0800

    整合座位分配軟體

---
+++ Random/randon-experiment.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <random>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    vector<int> seat = {};
+
+    for(int i=0;i<100;i++)
+        seat.push_back(i + 1);
+
+    //建立隨機設備
+    random_device rd;
+
+    //亂數產生器(梅森旋轉演算法)
+    mt19937 generator(rd());
+
+    shuffle(seat.begin(),seat.end(),generator);
+
+    fstream fp;
+    fp.open("Seat.txt",ios::out);
+    fp<<"ID,Seat"<<endl;
+
+    for(int i=0;i<100;i++){
+        fp<<i+1<<","<<seat[i]<<endl;
+    }
+
+    fp.close();
+    cout<<"Finished!"<<endl;
+}
